d4afa9a80f5f569533ddd2fb8cc89f4c91e14285
VIGNESHTHANGAVEL1 <vigneshtskv96@gmail.com>
2025-04-12 10:39:35 +0530

    code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-html
+// App.js
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
+
